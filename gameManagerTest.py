dd2fca1805e5b1690860ae20b0d68b6966905f3d
zenev <nevan.wichers@gmail.com>
2015-01-01 14:16:09 -0800

    minor changes

--- gameManagerTest.py
+++ gameManagerTest.py
@@ -10,27 +10,21 @@ from feedingGame.feedingGame3 import FeedingGame
 
 # gives random values for the joystick buttons and the position
 def get_input_mock_funct(*args, **kwargs):
-  joyPos = [random.random() * 2 -1, random.random() * 2 -1]
+    joyPos = [random.random() * 2 -1, random.random() * 2 -1]
 
-  triggered = [random.random() < .1,  random.random() < .05, random.random() < .01]
-  #triggered = [True, True, True]
+    triggered = [random.random() < .1,  random.random() < .05, random.random() < .01]
 
-  print("arg", args, "kw:", kwargs)
+    if (len(args) > 0 and args[0]) or ("polar" in kwargs and kwargs["polar"]):
+        joyPos = list(cart_to_polar(joyPos))
 
-  if (len(args) > 0 and args[0]) or ("polar" in kwargs and kwargs["polar"]):
-    print("polar")
-    joyPos = list(cart_to_polar(joyPos))
-
-  return joyPos, triggered
+    return joyPos, triggered
 
 while True:
-  gameManager = GameManager([(FeedingGame, ("")), (UseComputer, (""))], (1100, 650), .2)
+    gameManager = GameManager([(FeedingGame, ("")), (UseComputer, (""))], (1100, 650), .2)
 
-  input_mock = Mock()
-  input_mock.side_effect = get_input_mock_funct
-  gameManager.inputHandler.get_input = input_mock
+    input_mock = Mock()
+    input_mock.side_effect = get_input_mock_funct
+    gameManager.inputHandler.get_input = input_mock
 
-  #gameManager = GameManager([(FeedingGame, (""))], (1100, 750), .1)
-  #gameManager = GameManager([(UseComputer, (""))], (400, 400), .1)
-  gameManager.pay_select_game()
+    gameManager.pay_select_game()
 
